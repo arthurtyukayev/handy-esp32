58ea5ed13ffb4995a88094de6278aa86b43c3e1a
Arthur Tyukayev <github_arthur@tyukayev.com>
2025-08-07 12:40:55 -0700

    Added comments for keyboard shortcut

--- src/keyboard/keyboard.cpp
+++ src/keyboard/keyboard.cpp
@@ -4,10 +4,12 @@
 const InputReport NO_KEY_PRESSED = {};
 
 void sendKey() {
+  // Chnage the letter, the default is option + i / alt + i
   const char *i = "i";
   const uint RIGHT_ALT_KEY = 64;
   KEYMAP map = keymap[(uint8_t)*i];
 
+  // Remove or change the modifier, default is RIGHT_ALT_KEY / Right option key 
   InputReport report = {.modifiers = RIGHT_ALT_KEY,
                         .reserved = 0,
                         .pressedKeys = {map.usage, 0, 0, 0, 0, 0}};
